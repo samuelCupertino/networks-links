c6f4a19c4c1efd8c1e255735dfc77e1041859b4d
Samuel Cupertino <sacupertinno2014@gmail.com>
2021-09-23 05:05:38 -0300

    regexp router

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.set('views', './views')
 
 app.use(express.static(__dirname + '/views'))
 
-app.get('/[a-z]*', (req, res) => res.render('networks'))
+app.get('/[a-z]{0,}', (req, res) => res.render('networks'))
 
 app.listen(8080, ()=> console.log('server on!'))
