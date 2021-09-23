263636fc047fa84697f65bd1d014ab8d5a3a33af
Samuel Cupertino <sacupertinno2014@gmail.com>
2021-09-23 04:46:26 -0300

    add nodejs

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.use(express.static(__dirname + '/views'))
+
+app.get('/[a-z]*', (req, res) => res.render('networks'))
+
+app.listen(8080, ()=> console.log('server on!'))
